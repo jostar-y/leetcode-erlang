b4c442cf7d6c113d331b4877ec0d2e2580171f08
yr <1207595963@qq.com>
2021-11-24 17:39:36 +0800

    do

--- src/algorithms/algorithms_667.erl
+++ src/algorithms/algorithms_667.erl
@@ -12,9 +12,25 @@
 
 %% API
 -export([
-    do/1
+    construct_array/2
 ]).
 
-%% TODO
-do(_) ->
-    ok.
+-spec construct_array(N :: integer(), K :: integer()) -> [integer()].
+construct_array(N, K) ->
+    List = construct_array(1, N - K, []),
+    construct_array(N - K, N, N - K, N, 0, List).
+
+construct_array(I, N, Acc) when I < N ->
+    construct_array(I + 1, N, [I | Acc]);
+construct_array(_I, _N, Acc) ->
+    Acc.
+
+construct_array(I, N, Left, Right, J, Acc) when I =< N ->
+    case J rem 2 of
+        0 ->
+            construct_array(I + 1, N, Left + 1, Right, J + 1, [Left | Acc]);
+        _ ->
+            construct_array(I + 1, N, Left, Right - 1, J + 1, [Right | Acc])
+    end;
+construct_array(_, _, _, _, _, Acc) ->
+    lists:reverse(Acc).
