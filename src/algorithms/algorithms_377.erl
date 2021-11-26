0188d08d48aca0797c380540b08a5f59969093a9
yr <1207595963@qq.com>
2021-11-26 17:35:16 +0800

    do

--- src/algorithms/algorithms_377.erl
+++ src/algorithms/algorithms_377.erl
@@ -12,9 +12,21 @@
 
 %% API
 -export([
-    do/1
+    combination_sum4/2
 ]).
 
-%% TODO
-do(_) ->
-    ok.
+-spec combination_sum4(Nums :: [integer()], Target :: integer()) -> integer().
+combination_sum4(Nums, Target) ->
+    combination_sum4(Nums, Nums, Target, 1, #{0 => 1}).
+
+
+combination_sum4(_, _Nums, Target, I, Map) when Target + 1 == I ->
+    maps:get(Target, Map, 0);
+combination_sum4([], Nums, Target, I, Map) when I =< Target ->
+    combination_sum4(Nums, Nums, Target, I + 1, Map);
+combination_sum4([H | T], Nums, Target, I, Map) when I =< Target andalso H =< I ->
+    N1 = maps:get(I - H, Map, 0),
+    N2 = maps:get(I, Map, 0),
+    combination_sum4(T, Nums, Target, I, Map#{I => N1 + N2});
+combination_sum4([_H | T], Nums, Target, I, Map) when I =< Target ->
+    combination_sum4(T, Nums, Target, I, Map).
