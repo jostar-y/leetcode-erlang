209a66a2b3850c1030f568f008bb7e6ca73bb1d4
yr <1207595963@qq.com>
2021-11-30 11:42:46 +0800

    do

--- src/algorithms/algorithms_32.erl
+++ src/algorithms/algorithms_32.erl
@@ -12,9 +12,33 @@
 
 %% API
 -export([
-    do/1
+    longest_valid_parentheses/1
 ]).
 
-%% TODO
-do(_) ->
-    ok.
+-spec longest_valid_parentheses(S :: unicode:unicode_binary()) -> integer().
+longest_valid_parentheses(S) ->
+    S1 = binary_to_list(S),
+    Max = longest_valid_parentheses_l(S1, 0, 0, 0),
+    longest_valid_parentheses_r(lists:reverse(S1), 0, 0, Max).
+
+longest_valid_parentheses_l([], _, _, Max) ->
+    Max;
+longest_valid_parentheses_l([$) | S], Left, Right, Max) ->
+    if
+        Right + 1 == Left -> longest_valid_parentheses_l(S, Left, Left, max(Max, Left * 2));
+        Right + 1 > Left -> longest_valid_parentheses_l(S, 0, 0, Max);
+        true -> longest_valid_parentheses_l(S, Left, Right + 1, Max)
+    end;
+longest_valid_parentheses_l([$( | S], Left, Right, Max) ->
+    longest_valid_parentheses_l(S, Left + 1, Right, Max).
+
+longest_valid_parentheses_r([], _, _, Max) ->
+    Max;
+longest_valid_parentheses_r([$( | S], Left, Right, Max) ->
+    if
+        Left + 1 == Right -> longest_valid_parentheses_r(S, Right, Right, max(Max, Right * 2));
+        Left + 1 > Right -> longest_valid_parentheses_r(S, 0, 0, Max);
+        true -> longest_valid_parentheses_r(S, Left + 1, Right, Max)
+    end;
+longest_valid_parentheses_r([$) | S], Left, Right, Max) ->
+    longest_valid_parentheses_r(S, Left, Right + 1, Max).
